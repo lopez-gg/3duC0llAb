40096c68e678540e7357e4586be1f0c5d99a6d6f
lopez <maangelamalagueno.lopez@bicol-u.edu.ph>
2024-10-31 20:58:56 +0800

    +activity history @ add new sy

--- src/processes/a/add_sy.php
+++ src/processes/a/add_sy.php
@@ -1,31 +1,35 @@
 <?php
 require_once __DIR__ . '/../../config/db_config.php';
+require_once __DIR__ . '/../../config/session_config.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Get the year range from the POST request
     $yearRange = isset($_POST['yearRange']) ? $_POST['yearRange'] : null;
+    $userID = $_SESSION['user_id'];
 
     if ($yearRange) {
         try {
             // Check if the year range already exists in the `sy` table
-            $stmt = $pdo->prepare("SELECT id FROM sy WHERE year_range = ?");
+            $stmt = $pdo->prepare("SELECT sy_id FROM sy WHERE year_range = ?");
             $stmt->execute([$yearRange]);
             $existingRecord = $stmt->fetch(PDO::FETCH_ASSOC);
+            $sy_id = $stmt->fetch(PDO::FETCH_COLUMN);
 
             if ($existingRecord) {
-                // If the record exists, use the existing record
-                echo "Year range already exists. Using the existing record.";
+                // echo "Year range already exists. Using the existing record.";
             } else {
                 // If the record does not exist, insert the new year range
                 $stmt = $pdo->prepare("INSERT INTO sy (year_range) VALUES (?)");
                 $stmt->execute([$yearRange]);
 
-                echo "Year range saved successfully!";
+                $activity_message = '[ADDED] New SY Calendar: "' . $yearRange . '"';
+                add_activity_history($userID, $sy_id, $activity_message);
             }
         } catch (PDOException $e) {
             // Handle error
-            error_log('Error saving year range: ' . $e->getMessage());
-            echo "Failed to save year range.";
+            log_error('Error saving year range: ' . $e->getMessage(), 'db_errors.txt');
+
+            // echo "Failed to save year range.";
         }
     } else {
         echo "No year range provided.";
